963e8c8ee72c01230e332e2db5d42efb89f9b1fe
xiaohanchi <xiaohanchi123@gmail.com>
2025-04-07 22:35:13 -0500

    Add DoseStandardize function

--- R/dose_standardize.R
+++ R/dose_standardize.R
@@ -1,3 +1,13 @@
+#' Standardize Drug Dosage
+#'
+#' @param d Dose vectors for all considered doses
+#'
+#' @return Standardized doses
+#' @export
+#'
+#' @examples
+#' dosage_level <- c(300, 200, 0)
+#' DoseStandardize(dosage_level)
 DoseStandardize <- function(d) {
   return(d / max(d))
 }
